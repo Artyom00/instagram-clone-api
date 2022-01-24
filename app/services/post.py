7f0ed8387a917b5a2e5cb375708291157b115e98
Churin Dmitry <churin@10205.ru>
2022-01-24 15:57:18 +0300

    add logic for post deletion

--- app/services/post.py
+++ app/services/post.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from fastapi import status, HTTPException
 from sqlalchemy.orm import Session
 
 from app.db.models import Post
@@ -24,3 +25,19 @@ def create_post(db: Session, req_body: PostRequestBody):
 
 def get_posts_list(db: Session):
     return db.query(Post).all()
+
+
+def delete_post(db: Session, post_id: int, user_id: int):
+    post = db.query(Post).filter(Post.id == post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Post with id {post_id} not found')
+
+    if post.user_id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='Only post creator can delete post')
+
+    db.delete(post)
+    db.commit()
+
+    return {'message': 'Post deleted successfully'}
