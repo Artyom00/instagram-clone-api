3c8a6623ec75aa3e9519d42064e1157cec7b3dfc
Artyom Braynin <braynin.artyom@gmail.com>
2022-01-27 00:23:44 +0300

    add comment router

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.routers import user
-from app.routers import post
-from app.routers import authentication
+from app.routers import user, post, authentication, comment
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(authentication.router)
+app.include_router(comment.router)
 
 app.mount('/images', StaticFiles(directory='images'), name='images')
