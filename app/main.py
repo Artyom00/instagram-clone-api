737302756e40fe414bef1d7bd83cc1a1bce034f5
Churin Dmitry <churin@10205.ru>
2022-01-27 10:08:04 +0300

    add cors policy

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import user, post, authentication, comment
 
@@ -9,5 +10,17 @@ app.include_router(user.router)
 app.include_router(post.router)
 app.include_router(authentication.router)
 app.include_router(comment.router)
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 app.mount('/images', StaticFiles(directory='images'), name='images')
